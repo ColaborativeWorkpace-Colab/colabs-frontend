e9d612f08c4824635daa8a6495edcb687c6b9188
kalkidan12 <kalget12@gmail.com>
2023-03-17 00:11:22 +0300

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import Navigation from "./navigation/Navigation";
 
 import { Suspense } from "react";
-import Loading from "./components/Loder/Loding";
 export default function App() {
 	return (
 		<>
-			<Suspense fallback={<Loading />}>
-				<Navigation />
-			</Suspense>
+			<Navigation />
 		</>
 	);
 }

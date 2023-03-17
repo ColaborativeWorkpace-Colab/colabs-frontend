317ce9abf814a41da3dd5f34cbd07f0c791643c1
kalkidan12 <kalget12@gmail.com>
2023-03-17 14:25:26 +0300

    change auth nav color

--- src/pages/Auth/AuthHeader.jsx
+++ src/pages/Auth/AuthHeader.jsx
@@ -4,7 +4,7 @@ import logo from "../../assets/images/logo.png";
 
 const AuthHeader = ({ to, text, actionName }) => {
 	return (
-		<nav className="z-20 bg-white fixed w-full top-0 left-0 p-2">
+		<nav className="z-20 bg-slate-200 fixed w-full top-0 left-0 p-2">
 			<div className="flex flex-wrap justify-between">
 				<Link to="/" className="flex items-center">
 					<img src={logo} className="h-6 mr-3 sm:h-9" alt="kaliget Logo" />

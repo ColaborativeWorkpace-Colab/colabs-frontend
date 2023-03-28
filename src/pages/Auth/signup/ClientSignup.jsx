42c3f4eb7b1dedffa3110e9a94c8fd4d3c488b4e
kalkidan12 <kalget12@gmail.com>
2023-03-28 16:04:49 +0300

    er

--- src/pages/Auth/signup/ClientSignup.jsx
+++ src/pages/Auth/signup/ClientSignup.jsx
@@ -140,10 +140,7 @@ const ClientSignup = () => {
 										</div>
 									</div>
 								</div>
-								<button
-									type="submit"
-									className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
-								>
+								<button className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
 									Sign Up
 								</button>
 								<p className="text-sm font-light text-gray-500 dark:text-gray-400">

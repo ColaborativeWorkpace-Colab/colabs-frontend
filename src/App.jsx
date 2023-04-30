35c3596793c28de54a597915e97b5f502818a893
dawitt2327 <dawitt2327@gmail.com>
2023-05-01 00:13:47 +0300

    Adding Features remaining in SVT Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import ColabButton from "./utils/Button/ColabButton";
 import ColabCard from "./utils/Card/ColabCard";
 import { ColabLargeText, ColabMediumText } from "./utils/Typography/ColabText";
 import ColabInput from "./utils/TextField/ColabInput";
-import Svt from "./pages/User/Freelancer/Svt/Svt";
+
 
 export default function App() {
 	return (
 		<>
 			<Navigation />
-			<Svt /> 
+			
 		</>
 	);
 }

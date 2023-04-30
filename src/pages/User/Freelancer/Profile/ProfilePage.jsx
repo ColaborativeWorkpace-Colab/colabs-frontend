7b6bb176e85355d75f9cd1a99e4fb33489fc9437
dawitt2327 <dawitt2327@gmail.com>
2023-05-01 00:13:47 +0300

    Adding Features remaining in SVT Page

--- src/pages/User/Freelancer/Profile/ProfilePage.jsx
+++ src/pages/User/Freelancer/Profile/ProfilePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import FreelancerHeader from "../Header/Header";
+import Svt from "../Svt/Svt";
 
 const ProfilePage = () => {
 	return (
 		<>
 			<FreelancerHeader selectedNav={6} />
 			<div className="w-full h-full mt-[50px] px-12 py-8">Profile Page</div>;
+			<Svt />
+			
 		</>
 	);
 };

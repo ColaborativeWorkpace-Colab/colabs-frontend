f76184de569c90f2df40e72585eb1c14db3f8d4d
kalkidan12 <kalget12@gmail.com>
2023-06-11 00:05:38 +0300

    rtk query  .formik , some client page added

---
+++ src/pages/User/client/components/job/JobDetail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../dashboard/components/Header";
+import ClientHeader from "../../header/ClientHeader";
+
+const ClientJobDetail = () => {
+  return (
+    <div>
+      <ClientHeader />
+      <div></div>
+    </div>
+  );
+};
+
+export default ClientJobDetail;

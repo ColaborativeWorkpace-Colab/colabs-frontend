4d9ebf612528c830aabd0aeba9715b53afdb57ac
kalkidan12 <kalget12@gmail.com>
2023-06-11 03:04:15 +0300

    application-list and application-detail page added

--- src/pages/User/client/components/job/ApplicationDetail.jsx
+++ src/pages/User/client/components/job/ApplicationDetail.jsx
@@ -1,14 +1,65 @@
 import React from "react";
 import ClientHeader from "../../header/ClientHeader";
 import { AiOutlineFolderView } from "react-icons/ai";
-
+import profileImg from "../../../../../assets/images/profile.jpg";
+import { Link } from "react-router-dom";
 const ApplicationDetail = () => {
   return (
     <div>
       <ClientHeader />
       <div className="mt-[100px] md:px-[80px] px-[10px]">
-        <div className="shadow-sm flex gap-4 bg-gray-50 p-4">
-          /job-applications/:job-application-id/:user-id
+        <div className="shadow-sm flex flex-col gap-4 bg-gray-50 p-4">
+          <div className="w-full bg-gray-200 p-2 rounded-md flex justify-between items-center">
+            <div className="user-name flex gap-2">
+              <img
+                src={profileImg}
+                alt=""
+                className="rounded-full w-[50px] h-[50px]"
+              />
+              <div className="flex ml-2 flex-col">
+                <p className="text-lg text-slate-800">Kalkidan Getahun</p>
+                <p className="text-md text-slate-500">Frontend Developer</p>
+              </div>
+            </div>
+            <div className="">
+              <Link
+                to=""
+                className="text-white bg-purple-600 hover:bg-purple-700 rounded-md px-4 py-2"
+              >
+                View Kalkidan's Profile
+              </Link>
+            </div>
+          </div>
+          <div className="proposal content mt-2">
+            <p className="text-md text-slate-800">
+              Proposal Content Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Necessitatibus nostrum reiciendis rerum odio
+              aliquam libero delectus similique ratione assumenda sunt. Quia,
+              consequuntur. Dolorum blanditiis accusantium molestias cupiditate
+              qui at deleniti! Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Provident adipisci vel necessitatibus similique
+              vitae earum molestiae impedit, numquam voluptatibus, quae
+              perspiciatis consequuntur excepturi itaque aliquam facere quisquam
+              harum. At, eum. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit. Totam repellendus suscipit laborum animi culpa
+              alias rem ipsam quia accusantium, aut aspernatur? Facilis impedit
+              facere commodi quasi ducimus beatae voluptate sunt!
+            </p>
+          </div>
+          <div className="flex gap-x-5 mt-4">
+            <Link
+              to=""
+              className="text-white bg-purple-600 hover:bg-purple-700 rounded-md px-4 py-2"
+            >
+              Accept
+            </Link>
+            <Link
+              to=""
+              className="text-white bg-red-500 hover:bg-red-600 rounded-md px-4 py-2"
+            >
+              Reject
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/User/client/components/job/ApplicationList.jsx
+++ src/pages/User/client/components/job/ApplicationList.jsx
@@ -1,13 +1,53 @@
 import React from "react";
 import ClientHeader from "../../header/ClientHeader";
 import { AiOutlineFolderView } from "react-icons/ai";
+import {
+  MdOutlineArrowBackIosNew,
+  MdOutlineArrowForwardIos,
+} from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const ApplicationList = () => {
   return (
     <div>
       <ClientHeader />
       <div className="mt-[100px] md:px-[80px] px-[10px]">
-        <div className="shadow-sm flex gap-4 bg-gray-50 p-4"></div>
+        <div className="shadow-sm flex flex-col gap-4 bg-gray-50 p-4">
+          <div className="flex items-center gap-2 bg-gray-200 p-2 rounded-md">
+            <p className="text-purple-600">Proposals(10)</p>
+            <span className="text-md md:text-xl">-</span>
+            <p className="text-md md:text-xl">
+              Create Figma Design For Web Application
+            </p>
+          </div>
+          {[1, 2, 3, 4, 3].map((application, id) => (
+            <div className="border-b-[2px] border-gray-200 pb-1 flex items-center justify-between">
+              <p className="text-md text-slate-800">Kalkidan Getahun</p>
+              <div className="flex gap-2">
+                <Link
+                  to={"/job-applications/:job-application-id/:user-id"}
+                  className="bg-purple-600 hover:bg-purple-700 rounded-md px-3 py-2 text-white text-md text-slate-800"
+                >
+                  View
+                </Link>
+                <button className="bg-red-500 hover:bg-red-600 rounded-md px-3 py-2 text-white text-md text-slate-800">
+                  Decline
+                </button>
+              </div>
+            </div>
+          ))}
+          <div className="mt-2 flex gap-x-2 items-center">
+            <span className="cursor-pointer">
+              <MdOutlineArrowBackIosNew size={20} />
+            </span>
+            <p className="flex items-center justify-center rounded-md w-[25px] h-[25px] text-white text-md bg-gray-800">
+              1
+            </p>
+            <span className="cursor-pointer">
+              <MdOutlineArrowForwardIos size={20} />
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );

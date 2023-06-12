a913936713ee4cee7db2aa9ee8596c3d88504687
Daniel Demelash <danielddemissie@gmail.com>
2023-06-12 23:52:11 +0300

    Integrate backend with frontend

--- src/pages/Auth/signup/FreelancerSignup.jsx
+++ src/pages/Auth/signup/FreelancerSignup.jsx
@@ -5,39 +5,40 @@ import AuthHeader from "../AuthHeader";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { useSignupUserMutation } from "../../../services/auth/authApi";
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import validationSchema from "./validations";
+
 const FreelancerSignup = () => {
-  const navigate = useNavigate();
-  // const notify = () =>
-  //   toast("Signup successfully", {
-  //     type: "success",
-  //   });
+  const notify = (message, type) =>
+    toast(message, {
+      type,
+    });
 
-  const [user, setUser] = useState({
-    userType: "Freelancer",
-    email: "",
-    password: "",
-    firstName: "",
-    lastName: "",
-  });
-  const [
-    signupUser,
-    {
-      data: signupData,
-      isSuccess: isSignupSuccess,
-      error: signupError,
-      isError: isSignupError,
-    },
-  ] = useSignupUserMutation();
-  const handleSubmit = (user) => {
-    if (user.email && user.password) {
-      signupUser(user);
-      console.log(signupData, +"   " + isSignupSuccess);
+  const [signupUser, { isLoading }] = useSignupUserMutation();
 
-      // notify();
-      // setTimeout(() => {
-      //   navigate("/freelancer");
-      // }, 1500);
+  const handleSubmit = async (values) => {
+    try {
+      const response = await signupUser({
+        ...values,
+        userType: "Freelancer",
+      }).unwrap();
+
+      if (response) {
+        notify(response.message, "success");
+      }
+    } catch (error) {
+      notify(error.data.message, "error");
     }
+  };
+
+  const handlGoogleLogin = () => {
+    window.location.href =
+      "https://colabs-backend.vercel.app/api/v1/users/google?type=Employer";
+  };
+
+  const handlGithubLogin = () => {
+    window.location.href =
+      "https://colabs-backend.vercel.app/api/v1/users/github?type=Employer";
   };
   return (
     <>
@@ -53,180 +54,171 @@ const FreelancerSignup = () => {
               <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign up to find work you love
               </h1>
-              <form
+              <Formik
                 className="space-y-4 md:space-y-6"
-                action="#"
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  handleSubmit(user);
+                initialValues={{ email: "", password: "" }}
+                onSubmit={(values, actions) => {
+                  handleSubmit(values);
                 }}
+                validationSchema={validationSchema}
               >
-                <button
-                  aria-label="Continue with google"
-                  role="button"
-                  className="focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-700 py-3.5 px-4 border rounded-lg border-gray-700 flex items-center w-full mt-10"
-                >
-                  <img
-                    src="https://tuk-cdn.s3.amazonaws.com/can-uploader/sign_in-svg2.svg"
-                    alt="google"
-                  />
-                  <p className="text-base font-medium ml-4 text-gray-700">
-                    Continue with Google
-                  </p>
-                </button>
-                <button
-                  aria-label="Continue with github"
-                  role="button"
-                  className="focus:outline-none  focus:ring-2 focus:ring-offset-1 focus:ring-gray-700 py-3.5 px-4 border rounded-lg border-gray-700 flex items-center w-full mt-4"
-                >
-                  <img
-                    src="https://tuk-cdn.s3.amazonaws.com/can-uploader/sign_in-svg3.svg"
-                    alt="github"
-                  />
-                  <p className="text-base font-medium ml-4 text-gray-700">
-                    Continue with Github
-                  </p>
-                </button>
+                {({ values, errors, touched, handleChange, handleBlur }) => (
+                  <Form>
+                    <button
+                      aria-label="Continue with google"
+                      role="button"
+                      onClick={handlGoogleLogin}
+                      className="focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-gray-700 py-3.5 px-4 border rounded-lg border-gray-700 flex items-center w-full mt-10"
+                    >
+                      <img
+                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/sign_in-svg2.svg"
+                        alt="google"
+                      />
+                      <p className="text-base font-medium ml-4 text-gray-700">
+                        Continue with Google
+                      </p>
+                    </button>
+                    <button
+                      aria-label="Continue with github"
+                      role="button"
+                      onClick={handlGithubLogin}
+                      className="focus:outline-none  focus:ring-2 focus:ring-offset-1 focus:ring-gray-700 py-3.5 px-4 border rounded-lg border-gray-700 flex items-center w-full mt-4"
+                    >
+                      <img
+                        src="https://tuk-cdn.s3.amazonaws.com/can-uploader/sign_in-svg3.svg"
+                        alt="github"
+                      />
+                      <p className="text-base font-medium ml-4 text-gray-700">
+                        Continue with Github
+                      </p>
+                    </button>
 
-                <div className="w-full flex items-center justify-between py-5">
-                  <hr className="w-full bg-gray-400" />
-                  <p className="text-base font-medium leading-4 px-2.5 text-gray-400">
-                    OR
-                  </p>
-                  <hr className="w-full bg-gray-400  " />
-                </div>
-                <div className="flex gap-3 justify-between">
-                  <div className="grow">
-                    <label
-                      htmlFor="First Name"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      First Name
-                    </label>
-                    <input
-                      type="text"
-                      name="First Name"
-                      id="First Name"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="First Name"
-                      required
-                      onChange={(e) =>
-                        setUser({
-                          ...user,
-                          firstName: e.target.value,
-                        })
-                      }
-                      value={user.firstName}
-                    />
-                  </div>
-                  <div className="grow">
-                    <label
-                      htmlFor="Last Name"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      Last Name
-                    </label>
-                    <input
-                      type="text"
-                      name="Last Name"
-                      id="Last Name"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Last Name"
-                      required
-                      onChange={(e) =>
-                        setUser({
-                          ...user,
-                          lastName: e.target.value,
-                        })
-                      }
-                      value={user.lastName}
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@gmail.com"
-                    required
-                    onChange={(e) =>
-                      setUser({
-                        ...user,
-                        email: e.target.value,
-                      })
-                    }
-                    value={user.email}
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required
-                    onChange={(e) =>
-                      setUser({
-                        ...user,
-                        password: e.target.value,
-                      })
-                    }
-                    value={user.password}
-                  />
-                </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="text-purple-600 w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-purple-600 dark:ring-offset-gray-800"
+                    <div className="w-full flex items-center justify-between py-5">
+                      <hr className="w-full bg-gray-400" />
+                      <p className="text-base font-medium leading-4 px-2.5 text-gray-400">
+                        OR
+                      </p>
+                      <hr className="w-full bg-gray-400  " />
+                    </div>
+                    <div className="flex gap-3 justify-between my-3">
+                      <div className="grow">
+                        <label
+                          htmlFor="First Name"
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          First Name
+                        </label>
+
+                        <Field
+                          type="text"
+                          placeholder="Enter your first name"
+                          name="firstName"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                        <ErrorMessage
+                          name="firstName"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
+                      <div className="grow">
+                        <label
+                          htmlFor="Last Name"
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          Last Name
+                        </label>
+                        <Field
+                          type="text"
+                          placeholder="Enter Last Name"
+                          name="lastName"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                        <ErrorMessage
+                          name="lastName"
+                          component="div"
+                          className="text-red-500 text-sm"
+                        />
+                      </div>
+                    </div>
+                    <div className="my-3">
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Your email
+                      </label>
+                      <Field
+                        type="email"
+                        placeholder="Enter your email"
+                        name="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                      <ErrorMessage
+                        name="email"
+                        component="div"
+                        className="text-red-500 text-sm"
                       />
                     </div>
-                    <div className="ml-3 text-sm">
+                    <div className="my-3">
                       <label
-                        htmlFor="remember"
-                        className="text-gray-500 dark:text-gray-300"
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                       >
-                        Yes, I understand and agree to the Colabs Terms of
-                        Service , including the User Agreement and Privacy
-                        Policy .
+                        Password
                       </label>
+                      <Field
+                        type="password"
+                        placeholder="Enter your password"
+                        name="password"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="text-red-500 text-sm"
+                      />
                     </div>
-                  </div>
-                </div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
-                >
-                  Sign Up
-                </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Already have an account?{" "}
-                  <Link
-                    to={"/login"}
-                    className="font-medium text-purple-600 hover:underline dark:text-purple-700"
-                  >
-                    Log In
-                  </Link>
-                </p>
-              </form>
+                    <div className="flex items-center justify-between my-3">
+                      <div className="flex items-start">
+                        <div className="flex items-center h-5">
+                          <input
+                            id="remember"
+                            aria-describedby="remember"
+                            type="checkbox"
+                            className="text-purple-600 w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-purple-600 dark:ring-offset-gray-800"
+                          />
+                        </div>
+                        <div className="ml-3 text-sm">
+                          <label
+                            htmlFor="remember"
+                            className="text-gray-500 dark:text-gray-300"
+                          >
+                            Yes, I understand and agree to the Colabs Terms of
+                            Service , including the User Agreement and Privacy
+                            Policy .
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800 my-3"
+                    >
+                      {isLoading ? "Loading..." : "Sign Up"}
+                    </button>
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account?{" "}
+                      <Link
+                        to={"/login"}
+                        className="font-medium text-purple-600 hover:underline dark:text-purple-700"
+                      >
+                        Log In
+                      </Link>
+                    </p>
+                  </Form>
+                )}
+              </Formik>
             </div>
           </div>
         </div>

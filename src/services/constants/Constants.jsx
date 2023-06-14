a261b02bed6a436095c4a0d4c71d50d87d1720c5
Daniel Demelash <danielddemissie@gmail.com>
2023-06-15 00:20:57 +0300

    fix issues

--- src/services/constants/Constants.jsx
+++ src/services/constants/Constants.jsx
@@ -1,3 +1,3 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
-const BaseURL = "http://localhost:5000/api/v1/";
+const BaseURL = "https://colabs-backend.vercel.app/api/v1/";
 export const BaseQuery = fetchBaseQuery({ baseUrl: BaseURL });
